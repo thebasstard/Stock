c1c6a644ca77b3e76c6e30d2a4089cac60aeac10
ludo <ludo@LUDO>
2018-01-21 18:17:28 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Stock
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Saisissez le chemin complet du fichier :");
+			string chemin = Console.ReadLine();
+			Console.WriteLine();
+
+			// Création d'un stock avec seuil d'alerte à 50;
+			var stock = new Stock();
+			stock.SeuilAlerte = 50m;
+
+			// Abonnement à l'événement
+			stock.AlerteStockBas += Stock_AlerteStockBas;
+
+			// Chargement des mouvements de stock depuis le fichier
+			Console.WriteLine();
+			string ligne = String.Empty;
+			int cpt = 0;
+			try
+			{
+				// Lecture du fichier ligne à ligne
+				using (StreamReader str = new StreamReader(chemin))
+				{
+					DateTime jour;
+					decimal qté;
+					bool ligne1 = true;
+
+					Console.WriteLine("Création des mouvements de stocks");
+					while ((ligne = str.ReadLine()) != null)
+					{
+						if (ligne1)
+						{
+							ligne1 = false;
+							continue; // On écarte la première ligne car elle contient les en-têtes
+						}
+
+						jour = DateTime.Parse(ligne.Substring(0, 10));
+						qté = Decimal.Parse(ligne.Substring(11, ligne.Length - 11));
+
+						try
+						{
+							if (qté < 0)
+								stock.Retirer(jour, -qté);
+							else
+								stock.Ajouter(jour, qté);
+							cpt++;
+						}
+						catch (ArgumentException)
+						{
+							Console.WriteLine("Erreur : il y a déjà un mouvement de stock au {0}", jour.ToShortDateString());
+						}
+					}
+					Console.WriteLine("{0} mouvements de stocks créés", cpt);
+				}
+			}
+			catch (FileNotFoundException)
+			{
+				Console.WriteLine("Le fichier spécifié n'existe pas :\r\n{0}", chemin);
+			}
+
+			Console.WriteLine();
+
+			// Affichage de l'état du stock au 1er jour de chaque mois
+			for (var m = 1; m <= 12; m++)
+			{
+				var date = new DateTime(2016, m, 1);
+				Console.WriteLine("Etat du stock au {0} : {1} kg", date.ToShortDateString(), stock.GetEtatStock(date));
+			}
+
+			Console.ReadKey();
+		}
+
+		// Gestionnaire de l'évènement AlerteStockBas
+		private static void Stock_AlerteStockBas(object sender, DateAndDecimalEventArgs e)
+		{
+			Console.WriteLine("Attention, au {0}, il ne reste que {1} kg en stock !",
+				 e.Date.ToShortDateString(), e.DecimalValue);
+		}
+	}
+}
---
+++ Stock.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stock
+{
+    public enum TypesMouvements { Entrée, Sortie, RAZ }
+
+    class Stock
+    {
+        private SortedList<DateTime, Mouvement> _mouvements;
+
+        public decimal SeuilAlerte { get; set; }
+        public event EventHandler<DateAndDecimalEventArgs> AlerteStockBas;
+        public Stock()
+        {
+            _mouvements = new SortedList<DateTime, Mouvement>();
+        }
+
+        /// <summary>
+        /// Ajoute la quantité spécifiée au stock, à la date spécifiée
+        /// Lève une System.ArgumentException s'il y a déjà un mouvement de stock à la même date
+        /// </summary>
+        /// <param name="date"></param>
+        /// <param name="quantité"></param>
+        public void Ajouter(DateTime date, decimal quantité)
+        {
+            AjouterMvt(TypesMouvements.Entrée, date, quantité);
+        }
+
+        /// <summary>
+        /// Retire la quantité spécifiée du stock à la date spécifiée
+        /// Lève une System.ArgumentException s'il y a déjà un mouvement de stock à la même date
+        /// Lève une InvalidOperationException si la quantité en stock est insuffisante
+        /// </summary>
+        /// <param name="date"></param>
+        /// <param name="quantité"></param>
+        public void Retirer(DateTime date, decimal quantité)
+        {
+            // On vérifie si la quantité en stock est suffisante
+            // Si ce n'est pas le cas, on lève une exception
+            decimal etatStock = GetEtatStock(date);
+            if (quantité <= etatStock)
+                AjouterMvt(TypesMouvements.Sortie, date, quantité);
+            else
+                throw new InvalidOperationException("Quantité en stock insuffisante");
+
+            // Si la quantité en stock devient inférieure au seuil défini,
+            // on lève un évènement
+            if (etatStock < SeuilAlerte && AlerteStockBas != null)
+                AlerteStockBas(this, new DateAndDecimalEventArgs(date, etatStock));
+        }
+
+        /// <summary>
+        /// Remet le stock à zéro à la date spécifiée
+        /// </summary>
+        /// <param name="date"></param>
+        public void RemettreAZéro(DateTime date)
+        {
+            AjouterMvt(TypesMouvements.RAZ, date, 0);
+        }
+
+        /// <summary>
+        /// Obtient l'état du stock à une date donnée
+        /// </summary>
+        /// <param name="date">date de calcul du stock</param>
+        /// <returns>quantité en stock</returns>
+        public decimal GetEtatStock(DateTime date)
+        {
+            decimal qte = 0m;
+
+            foreach (var mvt in _mouvements)
+            {
+                if (mvt.Value.DateMvt <= date)
+                {
+                    switch (mvt.Value.Type)
+                    {
+                        case TypesMouvements.Entrée:
+                            qte += mvt.Value.Quantité;
+                            break;
+                        case TypesMouvements.Sortie:
+                            qte -= mvt.Value.Quantité;
+                            break;
+                        default:
+                            qte = 0;
+                            break;
+                    }
+                }
+                else
+                    break;
+            }
+
+            return qte;
+        }
+
+        private void AjouterMvt(TypesMouvements type, DateTime date, decimal qté)
+        {
+            var mvt = new Mouvement
+            {
+                Type = type,
+                DateMvt = date,
+                Quantité = qté
+            };
+
+            _mouvements.Add(date, mvt);
+        }
+
+        class Mouvement
+        {
+            public DateTime DateMvt { get; set; }
+            public decimal Quantité { get; set; }
+            public TypesMouvements Type { get; set; }
+        }
+    }
+
+    // Classe décrivant un argument d'évènement contenant une date et un décimal
+    public class DateAndDecimalEventArgs : EventArgs
+    {
+        public DateTime Date { get; set; }
+        public decimal DecimalValue { get; set; }
+        public DateAndDecimalEventArgs(DateTime date, decimal value)
+        {
+            Date = date;
+            DecimalValue = value;
+        }
+    }
+}
